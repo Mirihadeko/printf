d018ba603801bc6c746d2db3e4436ec2d76d4b56
Febyemad <febyemad14@gmail.com>
2023-08-20 07:49:53 -0700

    update to size.c

--- size.c
+++ size.c
@@ -2,7 +2,6 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * by Joseph and Ciny
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
